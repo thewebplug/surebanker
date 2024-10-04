1f047db634742959c31d90e0fc4e5611b7b0040b
saleem.jibril <saleemjibril5@gmail.com>
2024-10-04 10:37:43 +0100

    updated middleware again and again and again

--- middleware.js
+++ middleware.js
@@ -49,5 +49,5 @@ export function middleware(request) {
 }
 
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)']
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|/_next/image).*)']
 }
